410a36b5d7be5484475adcc2b404a37677b3e025
msemerni <msemerni@gmail.com>
2023-03-27 11:36:38 +0300

    update

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,13 @@ const { PORT, APP_NAME } = process.env;
 const port = PORT || 3001;
 
 const startApp = async () => {
-  const app = await NestFactory.create(AppModule, { abortOnError: false });
-  await app.listen(PORT);
-  console.log(`${APP_NAME} listen on port: ${port}`);
+  try {
+    const app = await NestFactory.create(AppModule, { abortOnError: false });
+    await app.listen(PORT);
+    console.log(`${APP_NAME} listen on port: ${port}`);
+  } catch (error) {
+    console.log(error.message);
+  }
 }
 
 startApp();
